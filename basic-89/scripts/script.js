1e8b3f6446d066451f7a550bed4a170a5411eaed
Sinostra <spanjaard.david@gmail.com>
2017-06-15 14:59:11 +0200

    exercice-ajax1

--- basic-89/scripts/script.js
+++ basic-89/scripts/script.js
@@ -25,4 +25,20 @@ $(function () {
 
 
 	}, changeFrequency);
+
+	var request = $.ajax({
+		url:"https://jsonplaceholder.typicode.com/users",
+		method:"GET",
+		dataType:"json"
+	});
+
+	request.done(function(data) {
+		for (var i = data.length - 1; i >= 0; i--) {
+			$('#names').append("<li>" + data[i].name + "</li>");
+		}
+
+		/*data.forEach(function(x){
+			console.log(x.name);
+		});*/
+	});
 });
